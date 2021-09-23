440a70ed6574460b7f57538560f5c67bad4c0f2a
Steven F <stevfabre@gmail.com>
2021-09-23 10:21:42 +0200

    Création du dropdown

---
+++ src/components/dropdown/Dropdown.js
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+import "./dropdown.css";
+import arrow from "../../img/arrow.svg";
+
+export default class Dropdown extends Component {
+  showDropdown(e) {
+    e.target.closest("div").children[1].classList.toggle("dropdown_arrow_hidden");
+    e.target.closest("div").children[2].classList.toggle("dropdown_hidden");
+  }
+
+  render() {
+    return (
+      <div className="dropdown_container" onClick={this.showDropdown}>
+        <h2 className="dropdown_title">{this.props.title}</h2>
+        <object className="dropdown_arrow dropdown_arrow_hidden" type="image/svg+xml" data={arrow} alt="dropdown_arrow">
+          {"Your browser does not support SVG "}
+        </object>
+        <p className="dropdown_description dropdown_hidden">{this.props.description}</p>
+      </div>
+    );
+  }
+}
